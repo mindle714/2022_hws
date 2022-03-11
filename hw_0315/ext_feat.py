7333b6814aad9bb9480e166e9080f0f037da733b
mindle714 <mindle714@naver.com>
2022-03-12 03:58:51 +0900

    add options

--- hw_0315/ext_feat.py
+++ hw_0315/ext_feat.py
@@ -2,6 +2,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--wav", type=str, required=True)
 parser.add_argument("--frame-sec", type=float, required=False, default=0.02)
+parser.add_argument("--shift-sec", type=float, required=False, default=0.01)
+parser.add_argument("--num-mel", type=int, required=False, default=128)
+parser.add_argument("--num-mfcc", type=int, required=False, default=20)
 parser.add_argument("--feat-type", type=str, required=False,
   default="logmel", choices=["logmel", "mfcc"])
 args = parser.parse_args()
@@ -17,12 +20,14 @@ basename = '.'.join(wave.split('/')[-1].split('.')[:-1])
 
 pcm, sr = soundfile.read(wave)
   
-f = np.abs(librosa.stft(pcm, n_fft=int(sr*args.frame_sec)))**2.
-mel = librosa.filters.mel(sr, int(sr*args.frame_sec))
+f = np.abs(librosa.stft(pcm, n_fft=int(sr*args.frame_sec), 
+  hop_length=int(sr*args.shift_sec), window='hamming'))**2.
+mel = librosa.filters.mel(sr, int(sr*args.frame_sec), n_mels=args.num_mel)
 f = np.dot(mel, f)
 
 if args.feat_type == "mfcc":
   db = 10. * np.log10(np.maximum(f, 1e-10))
   f = scipy.fftpack.dct(db, axis=0, type=2, norm='ortho')
+  f = f[:args.num_mfcc]
   
 np.save('{}_{}'.format(basename, args.feat_type), f)
