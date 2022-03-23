08899c6f43744762695ba42b2c9465fab0fbe89d
mindle714 <mindle714@naver.com>
2022-03-23 17:18:39 +0900

    add initial model.py

--- hw_0407/parse_data.py
+++ hw_0407/parse_data.py
@@ -1,15 +1,17 @@
 import tensorflow as tf
 
-def _parse_function(ex):
-  desc = {
-    'pcm': tf.io.FixedLenFeature([8192], tf.float32),
-    'speaker': tf.io.FixedLenFeature([1], tf.int64)
-  }
-  return tf.io.parse_single_example(ex, desc)
+def parse_func(pcm_len):
+  def _parse_func(ex):
+    desc = {
+      'pcm': tf.io.FixedLenFeature([pcm_len], tf.float32),
+      'speaker': tf.io.FixedLenFeature([1], tf.int64)
+    }
+    return tf.io.parse_single_example(ex, desc)
+  return _parse_func
 
-def gen_train(tfrec_list, batch_size=16):
+def gen_train(tfrec_list, pcm_len, batch_size=16):
   dataset = tf.data.TFRecordDataset(tfrec_list)
-  dataset = dataset.map(_parse_function)
+  dataset = dataset.map(parse_func(pcm_len))
   dataset = dataset.batch(batch_size)
   dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
   return dataset
