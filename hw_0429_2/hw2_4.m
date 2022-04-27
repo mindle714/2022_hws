140284cf639523c6290d79bd6801ad1cb16aeed2
mindle714 <mindle714@naver.com>
2022-04-28 03:25:28 +0900

    add intermediate

--- hw_0429_2/hw2_4.m
+++ hw_0429_2/hw2_4.m
@@ -1,10 +1,13 @@
 [img, map] = imread('noisy_image.png');
 
-img_wls = wls(img(:,:,1), .35, 1.2, 1e-4);
-%img_wls = wlsFilter(im2double(img(:,:,1)), .7, 1.2);
+%img_wls = zeros(size(img));
+img_wls = [wls(img(:, :, 1), .7, 1.2, 1e-4) ...
+    wls(img(:, :, 2), .7, 1.2, 1e-4) ...
+    wls(img(:, :, 3), .7, 1.2, 1e-4)];
+%img_wls = wlsFilter(im2double(img(1:4,1:4,1)), .7, 1.2);
 
 figure(1)
 subplot(1,2,1);
-imshow(img(:,:,1));
+imshow(img);
 subplot(1,2,2);
 imshow(img_wls);
--- hw_0429_2/wls.m
+++ hw_0429_2/wls.m
@@ -4,28 +4,29 @@ input = im2double(input);
 si = size(input);
 si_all = si(1) * si(2);
 
-dl_dx = diff(log(max(input, eps)), 1, 1);
-dl_dx = padarray(dl_dx, [1, 0], 'post');
+dl_dx = diff(log(max(input, eps)), 1, 2);
 a_x = abs(dl_dx).^alpha + eps;
 a_x = 1. ./ a_x;
+a_x = padarray(a_x, [0, 1], 'post');
 a_x = a_x(:);
+
+dl_dy = diff(log(max(input, eps)), 1, 1);
+a_y = abs(dl_dy).^alpha + eps;
+a_y = 1. ./ a_y;
+a_y = padarray(a_y, [1, 0], 'post');
+a_y = a_y(:);
 
 a_x_shift = padarray(a_x, si(1), 'pre');
 a_x_shift = a_x_shift(1:size(a_x));
-
-dl_dy = diff(log(max(input, eps)), 1, 2);
-dl_dy = padarray(dl_dy, [0, 1], 'post');
-a_y = abs(dl_dy).^alpha + eps;
-a_y = 1. ./ a_y;
-a_y = a_y(:);
 
 a_y_shift = padarray(a_y, 1, 'pre');
 a_y_shift = a_y_shift(1:size(a_y));
 
 lg_diag = a_x + a_x_shift + a_y + a_y_shift;
-lg_diag = lg_diag + 1 / lambda;
-lg = spdiags([a_x, a_y, lg_diag, a_y, a_x], [-si(1), -1, 0, 1, si(1)], si_all, si_all);
-lhs = lambda * lg;%eye(size(lg)) + lambda * lg;
+%lg = spdiags([a_x, a_y, lg_diag, a_y, a_x], [-si(1), -1, 0, 1, si(1)], si_all, si_all);
+lg = spdiags([-a_x, -a_y], [-si(1), -1], si_all, si_all);
+lg = lg + lg' + spdiags([lg_diag], [0], si_all, si_all);
+lhs = speye(si_all, si_all) + lambda * lg;
 rhs = input(:);
 
 output = lhs \ rhs;
