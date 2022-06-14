1a6fc1febc4ac3389fe2b66b3ceb888e3e947aea
mindle714 <mindle714@naver.com>
2022-06-14 17:15:37 +0900

    initial hough implementation

--- hw_0615/matlab/myHoughLines.m
+++ hw_0615/matlab/myHoughLines.m
@@ -1,5 +1,10 @@
 function [rhos, thetas] = myHoughLines(H, nLines)
-%Your implemention here
+H_flat = reshape(H,1,size(H,1) * size(H,2));
+[~, idx] = sort(H_flat, 'descend');
+idx = int32(idx);
 
-end
-        
+r_idx = mod((idx-1), size(H,1)) + 1;
+c_idx = idivide((idx-1), size(H,1)) + 1;
+
+thetas = reshape(double(r_idx(1:nLines)), nLines, 1);
+rhos = reshape(double(c_idx(1:nLines)), nLines, 1);
